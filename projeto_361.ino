bb11630afe2d55ec5a8654e21018f445667b8b4f
Eduardo Mariano <eduardomariano1810@gmail.com>
2024-12-17 20:00:49 -0300

    Add files via upload

---
+++ projeto_361.ino
@@ -0,0 +1,25 @@
+ // Projeto 36
+ #define sensorPin 0
+ float Celsius, Fahrenheit, Kelvin;
+ int sensorValue;
+ void setup() {
+ Serial.begin(9600);
+ Serial.println("Initialising.....");
+ }
+ void loop() {
+ GetTemp();
+ Serial.print("Celsius: ");
+ Serial.println(Celsius);
+ Serial.print("Fahrenheit: ");
+ Serial.println(Fahrenheit);
+ Serial.println();
+ delay(2000);
+ }
+ void GetTemp() {
+ sensorValue = analogRead(sensorPin);       
+
+ Kelvin = (((float(sensorValue) / 1023) * 5) * 100); 
+ Celsius = Kelvin - 273.15;            
+
+ Fahrenheit = (Celsius * 1.8) +32;         
+}
